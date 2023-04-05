c5faa88a56a0ff81b151e78cac56aaca9e9ca816
aoreo <hay6586@gmail.com>
2023-04-05 13:46:13 +0900

    before removing health connect

--- mobile/src/main/java/com/example/testsensor/MainActivity.kt
+++ mobile/src/main/java/com/example/testsensor/MainActivity.kt
@@ -15,13 +15,16 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     private val viewModel : MainViewModel by viewModels()
     private val dataClient by lazy { Wearable.getDataClient(this)}
     var isRunning = false
-    var changed=false
+    private lateinit var input : Array<Double>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,38 +34,17 @@ class MainActivity : AppCompatActivity() {
         val lineChart = findViewById<LineChart>(R.id.linechart)
         val addBtn = findViewById<Button>(R.id.addBtn)
 
-        var input = Array<Double>(5, { Math.random() })
-
         viewModel.hr.observe(this, Observer {
             text.text = viewModel.hr.value.toString()
+            addEntry(lineChart)
         })
 
-        if (lineChart.data!=null||text.text.toString()!="Hello World!") {
-            Log.d("changedValue",changed.toString())
-            val temp = viewModel.hr.value!!.toDouble()
-            input.plus(temp)
-            Log.d("helloworld!",temp.toString())
-            lifecycleScope.launch {
-                val data = lineChart.data
-                val dataSet = data.getDataSetByIndex(0)
-                for (i in input.indices) {
-                    if (i < dataSet.entryCount) {
-                        dataSet.getEntryForIndex(i).y = input[i].toFloat()
-                    } else {
-                        delay(100)
-                        dataSet.addEntry(Entry(i.toFloat(), input[i].toFloat()))
-                        data.notifyDataChanged()
-                    }
-                    lineChart.notifyDataSetChanged()
-                    lineChart.setVisibleXRangeMaximum(4f)
-                    lineChart.moveViewToX(dataSet.entryCount.toFloat())
-                    lineChart.invalidate()
-                }
-            }
-        }
             addBtn.setOnClickListener {
-                val temp = Array<Double>(5, { Math.random() })
-                input += temp
+                val temp = viewModel.hr.value!!.toDouble()
+                Log.d("dataare",temp.toString())
+                input=input.plus(temp)
+                Log.d("datais","${input[input.size-1]}  ${input.size} ")
+                Log.d("inputarray", input.contentToString())
                 lifecycleScope.launch {
                     val data = lineChart.data
                     val dataSet = data.getDataSetByIndex(0)
@@ -119,6 +101,46 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+    private fun addEntry(lineChart:LineChart){
+        val temp = viewModel.hr.value!!.toDouble()
+        Log.d("dataare",temp.toString())
+        if(::input.isInitialized){
+            input=input.plus(temp)
+        }else{
+            val entries = ArrayList<Entry>()
+            // Entry 배열 초기값 입력
+            entries.add(Entry(0F, 0F))
+            // 그래프 구현을 위한 LineDataSet 생성
+            val dataset = LineDataSet(entries, "input")
+            // 그래프 data 생성 -> 최종 입력 데이터
+            val data = LineData(dataset)
+            // chart.xml에 배치된 lineChart에 데이터 연결
+            lineChart.data = data
+
+            // 그래프 생성
+            lineChart.animateXY(1, 1)
+            input=Array<Double>(1,{temp})
+        }
+        lifecycleScope.launch {
+            val data = lineChart.data
+            val dataSet = data.getDataSetByIndex(0)
+            for (i in input.indices) {
+                if (i < dataSet.entryCount) {
+                    dataSet.getEntryForIndex(i).y = input[i].toFloat()
+                } else {
+                    delay(100)
+                    dataSet.addEntry(Entry(i.toFloat(), input[i].toFloat()))
+                    data.notifyDataChanged()
+                }
+                lineChart.notifyDataSetChanged()
+                lineChart.setVisibleXRangeMaximum(4f)
+                lineChart.moveViewToX(dataSet.entryCount.toFloat())
+                lineChart.invalidate()
+            }
+        }
+    }
+
     override fun onResume() {
         super.onResume()
         dataClient.addListener(viewModel)
